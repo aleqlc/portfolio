f6b4f70f1095c6539b9d673cd845609ce3a0fa89
최정환 <jjiillmm1@naver.com>
2020-08-12 22:08:41 +0900

    저장프로시저

---
+++ StoredProcedureWinform/StoredProcedure1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace StoredProcedure1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbox_id = new System.Windows.Forms.TextBox();
+            this.txtbox_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox_father = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_class = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F);
+            this.label1.Location = new System.Drawing.Point(149, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID : ";
+            // 
+            // txtbox_id
+            // 
+            this.txtbox_id.Location = new System.Drawing.Point(202, 97);
+            this.txtbox_id.Name = "txtbox_id";
+            this.txtbox_id.Size = new System.Drawing.Size(198, 25);
+            this.txtbox_id.TabIndex = 1;
+            // 
+            // txtbox_name
+            // 
+            this.txtbox_name.Location = new System.Drawing.Point(202, 140);
+            this.txtbox_name.Name = "txtbox_name";
+            this.txtbox_name.Size = new System.Drawing.Size(198, 25);
+            this.txtbox_name.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F);
+            this.label2.Location = new System.Drawing.Point(117, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name : ";
+            // 
+            // txtbox_father
+            // 
+            this.txtbox_father.Location = new System.Drawing.Point(202, 185);
+            this.txtbox_father.Name = "txtbox_father";
+            this.txtbox_father.Size = new System.Drawing.Size(198, 25);
+            this.txtbox_father.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 12F);
+            this.label3.Location = new System.Drawing.Point(113, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Father : ";
+            // 
+            // txtbox_class
+            // 
+            this.txtbox_class.Location = new System.Drawing.Point(202, 271);
+            this.txtbox_class.Name = "txtbox_class";
+            this.txtbox_class.Size = new System.Drawing.Size(198, 25);
+            this.txtbox_class.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 12F);
+            this.label4.Location = new System.Drawing.Point(119, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Class : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 12F);
+            this.label6.Location = new System.Drawing.Point(125, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "DOB : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 20F);
+            this.label7.Location = new System.Drawing.Point(97, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(333, 34);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Student Data Entery";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(202, 228);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(141, 323);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(119, 32);
+            this.btn_save.TabIndex = 14;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(281, 323);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(119, 32);
+            this.btn_delete.TabIndex = 15;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtbox_class);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbox_father);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbox_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbox_id);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbox_id;
+        private System.Windows.Forms.TextBox txtbox_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbox_father;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_class;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ StoredProcedureWinform/StoredProcedure1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StoredProcedure1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            SqlConnection ss = new SqlConnection("Data Source=DESKTOP-A5HOU30;Initial Catalog=StudentData;Integrated Security=True");
+            ss.Open();
+            SqlDataAdapter aa = new SqlDataAdapter("StudentInset",ss);
+
+            aa.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            aa.SelectCommand.Parameters.Add("@id",SqlDbType.Int).Value = txtbox_id.Text;
+            aa.SelectCommand.Parameters.Add("@Name", SqlDbType.VarChar,(50)).Value = txtbox_name.Text;
+            aa.SelectCommand.Parameters.Add("@Father", SqlDbType.VarChar,(50)).Value = txtbox_father.Text;
+            aa.SelectCommand.Parameters.Add("@DOB", SqlDbType.Date).Value = dateTimePicker1.Value.ToString();
+            aa.SelectCommand.Parameters.Add("@Class", SqlDbType.VarChar,(50)).Value = txtbox_class.Text;
+
+            aa.SelectCommand.ExecuteNonQuery();
+            ss.Close();
+            MessageBox.Show("저장되었습니다.");
+        }
+    }
+}
